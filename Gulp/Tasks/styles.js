4755a1528a559f48c3322523de5b5042291c587a
Akzreal <akzrealkino@gmail.com>
2018-03-02 13:23:11 +0530

    Mobile First

--- Gulp/Tasks/styles.js
+++ Gulp/Tasks/styles.js
@@ -4,13 +4,14 @@ var autoprefix = require('autoprefixer');
 var cssvars = require('postcss-simple-vars');
 var nested = require('postcss-nested');
 var cssimport = require('postcss-import');
+var mixin = require('postcss-mixins');
 
 
 
 
 gulptest.task( 'styles' , function(){
   return gulptest.src('./app/assets/styles/files.css')
-    .pipe(postcss([cssimport,cssvars, nested,autoprefix]))
+    .pipe(postcss([cssimport, mixin ,cssvars, nested,autoprefix]))
       .on('error' ,function(errorinfo) {
         console.log(errorinfo.toString());
         this.emit('end');
